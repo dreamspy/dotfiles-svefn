9afc4021ba367195e1a727518f82a5b673219d2c
Frímann Kjerúlf <frimann@Frimanns-MacBook-Pro-2.local>
2015-06-24 05:01:24 +0000

    Changed script to work with directories as well

--- symlinkDotFiles.sh
+++ symlinkDotFiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles-svefn                    # dotfiles directory
 olddir=~/dotfiles-svefn_old             # old dotfiles backup directory
-files="vimrc bashrc bashrc_functions gitconfig tmux.conf bash_aliases config oh-my-fish"    # list of files/folders to symlink in homedir
+files=".vimrc .bashrc .bashrc_functions .gitconfig .tmux.conf .bash_aliases .config .oh-my-fish"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -23,9 +23,13 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Deleting any existing dotfiles from ~ "
+
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    rm -rf ~/$file 
+done
+
+for file in $files; do
+	echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file/ ~/$file
 done
